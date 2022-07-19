756c0160278195d5621e8c6700d0affed5bf55d8
Bernardo <bernardojavier18@gmail.com>
2022-07-19 14:47:40 -0400

    Add click to play fuctionality to play queue

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-let videoSrc = document.getElementById("example");
-videoSrc.setAttribute("src", videoNames[5]);
-videoSrc.setAttribute("autoplay", "true");
+let mainVideo = document.getElementById("main-video");
+mainVideo.setAttribute("src", videoNames[5]);
+mainVideo.setAttribute("autoplay", "true");
 
 let queueVids = document.getElementById("queue-vids-container")
 
@@ -14,10 +14,15 @@ vid.setAttribute("height", "40px")
 
 let vidTitle = document.createElement("div")
 vidTitle.textContent = video
+vidTitle.addEventListener("click", setMainVideo)
 
 let queueVid = document.createElement("div")
 queueVid.appendChild(vid)
 queueVid.appendChild(vidTitle)
-
 queueVids.appendChild(queueVid)
 })
+
+
+function setMainVideo(){
+mainVideo.setAttribute("src", this.textContent)
+}

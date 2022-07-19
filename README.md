ee1d40dbd7fb301a55c2e1efa334e8bb7b2acfbf
Bernardo <bernardojavier18@gmail.com>
2022-07-18 21:40:57 -0400

    Create python script to generate file names. Create HTML skeleton

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # random-video-gallery
-Loads and formats a randomized selection of videos from a local directory into an HTML document
+Loads and formats a randomized selection of videos from a local directory 
+into an HTML document. Built for use with Firefox. Acceptable video file 
+types: .webm, .mp4
+
+1) Place collection of videos into the 'videos' folder
+2) Run the python script to generate a text file with a list of video file 
+names
+3) Open random-video-gallery.html
---
+++ file-name-generator.py
@@ -0,0 +1,16 @@
+import subprocess
+import re
+
+DIRECTORY = "./videos/"
+COMMAND = 'ls'
+
+## Generate a list of filenames from the given directory]
+dir_call = subprocess.run(COMMAND, text=True, shell=True, cwd=DIRECTORY, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+files = dir_call.stdout
+
+## Filter for filenames that end with .webm or .mp4
+matches = re.findall("\w+(?:\.webm|\.mp4)", files)
+
+with open("filenames.txt", mode="w") as filenames:
+    for match in matches:
+        print(match, file=filenames)

c718d11abf2ab8f906aeb44e5af814170bcb3f4b
Bernardo <bernardojavier18@gmail.com>
2022-07-19 14:25:07 -0400

    Create play queue structure

--- javascript.js
+++ javascript.js
@@ -1,2 +1,23 @@
 let videoSrc = document.getElementById("example");
-videoSrc.setAttribute("src", videoNames[5])
+videoSrc.setAttribute("src", videoNames[5]);
+videoSrc.setAttribute("autoplay", "true");
+
+let queueVids = document.getElementById("queue-vids-container")
+
+videoNames.forEach(video => {
+
+let vid = document.createElement("video")
+vid.setAttribute("src", video)
+vid.setAttribute("type", "video/webm")
+vid.setAttribute("preload", "metadata")
+vid.setAttribute("height", "40px")
+
+let vidTitle = document.createElement("div")
+vidTitle.textContent = video
+
+let queueVid = document.createElement("div")
+queueVid.appendChild(vid)
+queueVid.appendChild(vidTitle)
+
+queueVids.appendChild(queueVid)
+})

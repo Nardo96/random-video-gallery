baddfcec7676d16cc4c8abe0c4e5a74715f572b5
Bernardo <bernardojavier18@gmail.com>
2022-07-19 16:01:14 -0400

    Replace array forEach with for loop to implement break

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 // Shuffle video names array
 for (let i = videoNames.length - 1; i > 0; i--){
-    const j = Math.floor(Math.random() * (i+1));
-    [videoNames[i], videoNames[j]] = [videoNames[j], videoNames[i]]
+  const j = Math.floor(Math.random() * (i+1));
+  [videoNames[i], videoNames[j]] = [videoNames[j], videoNames[i]]
 }
 
 let mainVideo = document.getElementById("main-video");
@@ -12,25 +12,48 @@ mainVideo.addEventListener("ended", playNext);
 
 let queueVids = document.getElementById("queue-vids-container")
 
-videoNames.forEach(video => {
+const VIDEOLIMIT = 9;
+let vidCounter = 0;
 
-let vid = document.createElement("video")
-vid.setAttribute("src", video)
-vid.setAttribute("type", "video/webm")
-vid.setAttribute("preload", "metadata")
-vid.setAttribute("height", "40px")
-if (video.endsWith(".webm")) vid.setAttribute("type", "video/webm")
-else if (video.endsWith(".mp4")) vid.setAttribute("type", "video/mp4")
+for (let i = 0; i < videoNames.length; i++){
+	let vid = document.createElement("video")
+	vid.setAttribute("src", videoNames[i])
+	vid.setAttribute("type", "video/webm")
+	vid.setAttribute("preload", "metadata")
+	vid.setAttribute("height", "40px")
+	if (videoNames[i].endsWith(".webm")) vid.setAttribute("type", "video/webm")
+	else if (videoNames[i].endsWith(".mp4")) vid.setAttribute("type", "video/mp4")
 
-let vidTitle = document.createElement("div")
-vidTitle.textContent = video
-vidTitle.addEventListener("click", setMainVideo)
+	let vidTitle = document.createElement("div")
+	vidTitle.textContent = videoNames[i]
+	vidTitle.addEventListener("click", setMainVideo)
 
-let queueVid = document.createElement("div")
-queueVid.appendChild(vid)
-queueVid.appendChild(vidTitle)
-queueVids.appendChild(queueVid)
-})
+	let queueVid = document.createElement("div")
+	queueVid.appendChild(vid)
+	queueVid.appendChild(vidTitle)
+	queueVids.appendChild(queueVid)
+	
+	if (++vidCounter == VIDEOLIMIT) break
+	
+}
+/* videoNames.forEach(video => {
+	let vid = document.createElement("video")
+	vid.setAttribute("src", video)
+	vid.setAttribute("type", "video/webm")
+	vid.setAttribute("preload", "metadata")
+	vid.setAttribute("height", "40px")
+	if (video.endsWith(".webm")) vid.setAttribute("type", "video/webm")
+	else if (video.endsWith(".mp4")) vid.setAttribute("type", "video/mp4")
+
+	let vidTitle = document.createElement("div")
+	vidTitle.textContent = video
+	vidTitle.addEventListener("click", setMainVideo)
+
+	let queueVid = document.createElement("div")
+	queueVid.appendChild(vid)
+	queueVid.appendChild(vidTitle)
+	queueVids.appendChild(queueVid)
+}) */
 
 
 function setMainVideo(){
@@ -38,9 +61,9 @@ mainVideo.setAttribute("src", this.textContent)
 }
 
 function playNext(){
-    index = videoNames.findIndex(item => item == this.getAttribute("src"))
-    next = index + 1
-    setTimeout( () => mainVideo.setAttribute("src", videoNames[next]), 500)
+  index = videoNames.findIndex(item => item == this.getAttribute("src"))
+  next = index + 1
+	setTimeout( () => mainVideo.setAttribute("src", videoNames[next]), 500)
     
 
 }
